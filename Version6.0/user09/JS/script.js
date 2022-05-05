c9f5acaac6e71e10b2f3eb72014dc5094cd37639
308932 <103434643+308932@users.noreply.github.com>
2022-05-04 19:09:33 -0500

    6.0b

---
+++ Version6.0/user09/JS/script.js
@@ -0,0 +1,81 @@
+function changeText() {
+    document.getElementById("textChange").innerHTML="Thanks for liking my Webpage";
+   document.getElementById("Change").innerHTML="Thanks for liking my Webpage";
+   }
+
+   var space = " ";
+   var pos = 0;
+   var msg = "User 19";
+   
+   function Scroll(){
+   document.title = msg.substring(pos, msg.length) + space +msg.substring(0,pos);
+   
+   pos++;
+   if (pos > msg.length) pos = 0;
+   window.setTimeout("Scroll()", 0);
+   }
+   Scroll();
+   
+   function myFunction() {
+    var x = document.getElementById("fname");
+    x.value = x.value.toUpperCase();
+  }
+
+  function myFunction2(e) {
+    x = e.clientX;
+    y = e.clientY;
+    coor = "Coordinates: (" + x + "," + y + ")";
+    document.getElementById("demo").innerHTML = coor;
+  }
+
+  function clearCoor() {
+    document.getElementById("demo").innerHTML = "";
+  }
+
+  function bigImg(x) {
+    x.style.height = "120px";
+    x.style.width = "120px";
+  }
+  
+  function normalImg(x) {
+    x.style.height = "90px";
+    x.style.width = "90px";
+  }
+
+  function preferedBrowser() {
+    prefer = document.forms[0].browsers.value;
+    alert("You prefer browsing internet with " + prefer);
+  }
+
+  function preferedBrowser() {
+    prefer = document.forms[0].browsers.value;
+    alert("You prefer browsing internet with " + prefer);
+  }
+
+  function loadImage() {
+    alert("Image is loaded");
+  }
+
+  function confirmInput() {
+    fname = document.forms[0].fname.value;
+    alert("Hello " + fname + "! You will now be redirected to www.w3Schools.com");
+  }
+
+  function myFunction(elmnt, clr) {
+    elmnt.style.color = clr;
+  }
+
+  function whichElement(e) {
+    var targ;
+    if (!e) {
+      var e = window.event;
+    }
+    if (e.target) {
+      targ=e.target;
+    } else if (e.srcElement) {
+      targ=e.srcElement;
+    }
+    var tname;
+    tname = targ.tagName;
+    alert("You clicked on a " + tname + " element.");
+  }
