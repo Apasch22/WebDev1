11782be5dcf50f2992cb7027cab247216cbdf0a5
309986 <103433974+309986@users.noreply.github.com>
2022-05-16 09:50:27 -0500

    1.7
    Have added all the photos and added new file so every site has 2 pictures now trying to get the script to work and add more of them

---
+++ Version7.0/user03/JS/SampleJS.js
@@ -0,0 +1,7 @@
+<body>
+<button type="button" onclick="myFunction()">More Habitat information</button>
+<p id="demo"></p>
+<script>function myFunction() { 
+  document.getElementById("demo").innerHTML = "Hello JavaScript!"
+}</script>
+</body>
