ba012d7d3ebdd9e22e2240db0a59837f615f9290
309986 <103433974+309986@users.noreply.github.com>
2022-05-16 22:27:33 -0500

    1.7
    Finished the scrips.

--- Version7.0/user03/JS/SampleJS.js
+++ Version7.0/user03/JS/SampleJS.js
@@ -1,7 +1,40 @@
+<html lang="en">
+<head>
+<script>
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Hello World"}
+</script>
+
+<script>
+function myFunction(elmnt, clr) {
+  elmnt.style.color = clr
+}
+</script>
+
+<script>
+        function bigImg(x) {
+          x.style.height = "375px";
+          x.style.width = "400px";
+        }
+        
+        function normalImg(x) {
+          x.style.height = "225px";
+          x.style.width = "250px";
+        }
+        </script>
+
+</head>
 <body>
-<button type="button" onclick="myFunction()">More Habitat information</button>
+
+
+<button onclick="myFunction()">More Information</button>
+
 <p id="demo"></p>
-<script>function myFunction() { 
-  document.getElementById("demo").innerHTML = "Hello JavaScript!"
-}</script>
+
+<h5 onmouseover="style.color='red'" onmouseout="style.color='black'">Habitat</h5>
+<h5 onmouseover="style.color='blue'" onmouseout="style.color='black'">Why they matter</h5>
+
+<h5 onmousedown="myFunction(this,'yellow')" onmouseup="myFunction(this,'purple')">Habitat</h5>
+<h5 onmousedown="myFunction(this,'orange')" onmouseup="myFunction(this,'turquoise')">Why they matter</h5>
 </body>
+</html>
