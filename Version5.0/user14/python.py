de78f3a06fb053ee31ec39400efd6d3a28ae2b2d
302494 <302494@shakopeeschools.org>
2022-06-02 08:45:13 -0500

    4.3 PHP eBay

--- Version5.0/user14/python.py
+++ Version5.0/user14/python.py
@@ -18,3 +18,30 @@ else:
 tup = (12, "String", True, 57, False)
 
 print(type(tup))
+
+for x in range(1,101):
+    if x % 3 == 0:
+        print("Fizz")
+    elif x % 5 == 0:
+        print("Buzz")
+    elif x % 3 and x % 5 == 0:
+        print("FizzBuzz")
+    else:
+        print(x)
+
+for x in range(1,101):
+    fizz = "Fizz" if x % 3 == 0 else ""
+    buzz = "Buzz" if x % 5 == 0 else ""
+    print(f{fizz}{buzz} or x)
+
+f = open("pythontest.txt", "a")
+f.write("File Content")
+print(f.read())
+f.close()
+
+def lowercase(string):
+    string.lower()
+
+name = "DERRICK"
+age = "68"
+print(f"{lowercase(name)} is {age} years old")
