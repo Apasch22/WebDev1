2712165f25823153d9ac2d1de66b6ef518b45233
302379 <103434042+302379@users.noreply.github.com>
2022-06-08 19:33:00 -0500

    302379
    June 8th

--- Version6.0/user04/script.js
+++ Version6.0/user04/script.js
@@ -2,3 +2,20 @@ function changeText() {
   document.getElementById("textChange").innerHTML =
     "Thanks for liking my Webpage";
 }
+
+// Didn't work (step 11) //
+var space = " ";
+var pos = 0;
+var msg = "User 19";
+
+function Scroll() {
+  document.title =
+    msg.substring(pos, msg.length) + space + msg.substring(0, pos);
+
+  pos++;
+  if (pos > msg.length) pos = 0;
+  window.setTimeout("Scroll()", 0);
+}
+Scroll();
+// Didn't work (step 11) //
+
